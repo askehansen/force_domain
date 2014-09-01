73efb111f826794e5ffa5fdfa6af82d5c66eec55
Aske Hansen <aske@deeco.dk>
2014-09-01 15:16:12 +0200

    fix typo

--- lib/force_domain/railtie.rb
+++ lib/force_domain/railtie.rb
@@ -1,7 +1,7 @@
 module ForceDomain
   class Railtie < Rails::Railtie
     initializer "force_domain.configure_rails_initialization" do
-      Rails.application.middleware.use EnsureDomain::Middleware
+      Rails.application.middleware.use ForceDomain::Middleware
     end
   end
 end
