e7719085c5c70c4fd6fc6736598806449fb3523f
Aske Hansen <aske@deeco.dk>
2016-06-16 14:27:10 +0200

    insert middle a the top

--- lib/force_domain/railtie.rb
+++ lib/force_domain/railtie.rb
@@ -1,7 +1,7 @@
 module ForceDomain
   class Railtie < Rails::Railtie
     initializer "force_domain.configure_rails_initialization" do
-      Rails.application.middleware.use ForceDomain::Middleware
+      Rails.application.middleware.insert_before 0, ForceDomain::Middleware
     end
   end
 end
